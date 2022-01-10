45d7c7639810280e75d8ce8764c592fc05f2f783
pliupelis <pliupelis@inbox.lt>
2022-01-10 06:39:09 +0200

    1h to go

---
+++ src/js/colorChange.js
@@ -0,0 +1,12 @@
+// const types = document.querySelectorAll("option");
+// const colorChange = () => {
+//   types.forEach((type, i) => {
+//     type.label == "green" ? (type.style.backgroundColor = type.label) : "";
+//     type.label == "yellow" ? (type.style.backgroundColor = type.label) : "";
+//     type.label == "blue" ? (type.style.backgroundColor = type.label) : "null";
+//     console.log(type.label);
+//     console.log(type);
+//   });
+// };
+
+// colorChange();
